9a72a713e11cff1ecd6eff8686f27f74ea443e87
lingrui <lingrui@stbl.cc>
2017-01-10 09:27:45 +0800

    提交代码

--- vendor/app/controller/file.go
+++ vendor/app/controller/file.go
@@ -19,20 +19,14 @@ func init() {
 func upload(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	var err error
-	_, err = os.Create("test.jpg")
+	file, err := os.Create("test.jpg")
 	if err != nil {
 		log.Println(err)
 		response.SendError(w, errcode.ServerError)
 		return
 	}
-	f, err := os.OpenFile("test.jpg", os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		log.Println(err)
-		response.SendError(w, errcode.ServerError)
-		return
-	}
-	defer f.Close()
-	nbytes, err := io.Copy(f, r.Body)
+	defer file.Close()
+	nbytes, err := io.Copy(file, r.Body)
 	if err != nil {
 		log.Println(err)
 		response.SendError(w, errcode.ServerError)
